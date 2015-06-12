23eaefe7d9abd4f82e22389c04f3332f088e3fbc
Iago Abal <mail@iagoabal.eu>
2015-06-12 12:01:28 +0200

    Update test/6387.c

--- test/6387.c
+++ test/6387.c
@@ -1,4 +1,3 @@
-
 extern void __assert_fail (const char *__assertion, const char *__file,
       unsigned int __line, const char *__function)
      __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
@@ -9,54 +8,59 @@ static int* _omap3_sram_configure_core_dpll;
 
 int omap3_configure_core_dpll()
 {
-  __assert_fail ("_omap3_sram_configure_core_dpll", "63878ac.c", 12, __PRETTY_FUNCTION__);
-  return *_omap3_sram_configure_core_dpll;
+ ((_omap3_sram_configure_core_dpll)
+ ? (void) (0)
+ : __assert_fail ("_omap3_sram_configure_core_dpll", "63878ac.c", 16, __PRETTY_FUNCTION__));
+ return *_omap3_sram_configure_core_dpll;
 }
 
 int omap3_core_dpll_m2_set_rate()
 {
-	omap3_configure_core_dpll(); // (5)
-	return 0;
+ omap3_configure_core_dpll();
+ return 0;
 }
 
-static int _omap2_init_reprogram_sdrc(void)
+int _omap2_init_reprogram_sdrc(void)
 {
-	int v;
+ int v;
 
-	v = omap3_core_dpll_m2_set_rate(); // (4)
+ v = omap3_core_dpll_m2_set_rate();
 
-	return v;
+ return v;
 }
 
-static inline int omap34xx_sram_init(void)
+
+int omap34xx_sram_init(void)
 {
-	return 0;
+ return 0;
 }
 
 int omap_sram_init(void)
 {
-	if (nondet())
-		omap34xx_sram_init();
+ if (nondet())
+  omap34xx_sram_init();
 
-	return 0;
+ return 0;
 }
 
 void omap_sdrc_init()
 {
-	omap_sram_init(); // does nothing in ARCH_OPAM3 && !PM
+ omap_sram_init();
 
-	if (nondet()) { // evaluates to true
-		_omap2_init_reprogram_sdrc(); // (3)
-	}
+ if (nondet()) {
+
+  _omap2_init_reprogram_sdrc();
+
+ }
 }
 
-static void omap3pandora_init(void)
+void omap3pandora_init(void)
 {
-	omap_sdrc_init(); // (2)
+ omap_sdrc_init();
 }
 
 int main(void)
 {
-	omap3pandora_init(); // (1)
-	return 0;
+ omap3pandora_init();
+ return 0;
 }
